0ad0b48721832418df36d10e35e46610b38c5c43
imzhangkebetter <zhangkebetter@gmail.com>
2016-09-08 11:45:01 +0800

    车库修改提交

--- src/main/java/com/ztw/car/iservice/ICarService.java
+++ src/main/java/com/ztw/car/iservice/ICarService.java
@@ -2,10 +2,16 @@ package com.ztw.car.iservice;
 
 import com.ztw.car.model.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by zxt on 2016/9/7.
  */
 public interface ICarService extends JpaRepository<Car,Integer> {
-    public void updateCarName(){}
+    @Modifying
+    @Transactional
+    @Query("update Car c set c.carName= ?2 where c.id= ?1")
+    public void updateCarName(Integer id,String carName);
 }
