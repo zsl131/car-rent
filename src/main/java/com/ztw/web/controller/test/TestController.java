e1e37f1de02fda70b957cd169a851b8419547070
zsl131 <398986099@qq.com>
2016-09-12 12:03:36 +0800

    删除Mongo相关东西

--- src/main/java/com/ztw/web/controller/test/TestController.java
+++ src/main/java/com/ztw/web/controller/test/TestController.java
@@ -1,16 +1,7 @@
 package com.ztw.web.controller.test;
 
-import com.mongodb.MongoClient;
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.gridfs.GridFSBucket;
-import com.mongodb.client.gridfs.GridFSBuckets;
-import com.mongodb.client.gridfs.model.GridFSUploadOptions;
-import com.ztw.basic.db.MongDataSourceConfig;
-import com.ztw.basic.test.MongoUserService;
 import com.ztw.basic.tools.ConfigTools;
 import org.apache.commons.io.FileUtils;
-import org.bson.Document;
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.*;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by zsl-pc on 2016/9/8.
@@ -31,41 +24,10 @@ public class TestController {
     @Autowired
     ConfigTools configTools;
 
-    @Autowired
-    MongoUserService mongoUserService;
-
-    @Autowired
-    MongDataSourceConfig mongDataSourceConfig;
-
     //http://www.cnblogs.com/shihuc/p/5169418.html
     @RequestMapping(value = "index", method = RequestMethod.GET)
     public String index() throws Exception {
         System.out.println("=============="+configTools.getUploadPath());
-
-        /*MongoUser user = new MongoUser();
-        user.setUsername("zsl");
-        user.setAge(28);
-        user.setHometown("yunnan");
-        user.setJob("coder");
-
-        mongoUserService.addUser(user);*/
-
-        MongoClient client = mongDataSourceConfig.buildMongo();
-        MongoDatabase database = client.getDatabase("car-rent");
-        GridFSBucket gridFSBucket = GridFSBuckets.create(database);
-
-//        GridFSBucket gridFSBucket = GridFSBuckets.create(database, "files");
-
-        InputStream streamToUploadFrom = new FileInputStream(new File("d:/temp/eureka-server-1.0-SNAPSHOT.jar"));
-
-// Create some custom options
-        GridFSUploadOptions options = new GridFSUploadOptions()
-                .chunkSizeBytes(1024)
-                .metadata(new Document("type", "presentation"));
-
-        ObjectId fileId = gridFSBucket.uploadFromStream("mongodb-tutorial", streamToUploadFrom, options);
-
-        System.out.println(fileId.toHexString());
 
         return "test/index";
     }
