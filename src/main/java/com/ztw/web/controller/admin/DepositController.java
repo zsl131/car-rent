1e0f62bcd572ef401b809fe20ffb043ba5eb8123
silentmx <maxu0213@outlook.com>
2016-09-14 10:02:46 +0800

    写了保证金管理按身份证查询的功能,其他功能有待进一步完善

--- src/main/java/com/ztw/web/controller/admin/DepositController.java
+++ src/main/java/com/ztw/web/controller/admin/DepositController.java
@@ -1,10 +1,17 @@
 package com.ztw.web.controller.admin;
 
 import com.ztw.basic.auth.annotations.AdminAuth;
+import com.ztw.basic.tools.PageableTools;
+import com.ztw.deposit.model.Deposit;
+import com.ztw.deposit.service.IDepositService;
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * 保证金管理controller
@@ -15,9 +22,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @AdminAuth(name = "保证金", orderNum = 10, psn = "系统管理", pentity=0, porderNum=20)
 public class DepositController {
 
-    @AdminAuth(name="保证金管理", orderNum=1, icon="glyphicon glyphicon-usd")
+    @Autowired
+    private IDepositService depositService;
+
+    @AdminAuth(name="保证金管理", orderNum=1, icon="icon-usd")
     @RequestMapping(value="index", method= RequestMethod.GET)
-    public String index(HttpServletRequest request) {
+    public String index(HttpServletRequest request, Model model, Integer page,
+                        @RequestParam(name = "tenantSfz", defaultValue = "") String tenantSfz) {
+
+        Page<Deposit> datas;
+        if(!tenantSfz.equals("")) {
+            datas = depositService.pageByTenantSfz(tenantSfz, PageableTools.basicPage(page));
+        } else {
+            datas = depositService.pageAll(PageableTools.basicPage(page));
+        }
+
+        model.addAttribute("datas", datas);
+        model.addAttribute("tenantSfz", tenantSfz);
         return "admin/deposit/index";
     }
 
