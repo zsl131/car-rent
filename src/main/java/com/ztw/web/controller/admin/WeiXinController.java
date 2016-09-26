81f3c8196d75034c9341bba652bbde8b67f51a20
imzhangkebetter <zhangkebetter@gmail.com>
2016-09-26 12:26:24 +0800

    微信管理修改了自定义菜单json数据报错的问题

--- src/main/java/com/ztw/web/controller/admin/WeiXinController.java
+++ src/main/java/com/ztw/web/controller/admin/WeiXinController.java
@@ -131,7 +131,7 @@ public class WeiXinController {
         String result = WeiXinMenuTool.createWeiXinMenuJson(
                 BeanFactoryContext.getAccessToken(), WeixinUtil.createWeiXinMenu(weiXinMenuService));
         Message meg = JSON.parseObject(result,Message.class);
-        if(meg.getErrcode().equals("ok")){
+        if(meg.getErrmsg().equals("ok")){
             return "success";
         }else{
             return "错误代码："+meg.getErrcode();
